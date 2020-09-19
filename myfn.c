62c02b561dbea6c2bd4c96ebe8aaa212971702e9
99002589 <moulesh.thangavelu@ltts.com>
2020-09-19 09:03:42 +0530

    palindrome

--- myfn.c
+++ myfn.c
@@ -29,3 +29,20 @@ int symbol(int no)
   else if(no==0)
     return 0;
 }
+
+int palindrome(int no)
+{
+  int rev=0, tmp=0, rem=0;
+  tmp=no;
+  while(tmp>0)
+  {
+    rem=tmp%10;
+    rev=(rev*10)+10;
+    tmp=tmp/10;
+  }
+
+  while (n != 0) {
+        remainder = n % 10;
+        reversedN = reversedN * 10 + remainder;
+        n /= 10;
+}
--- myfn.h
+++ myfn.h
@@ -1,1 +1,6 @@
-#ifndef
+#ifndef __MYFN_H__
+#define __MYFN_H__
+
+int odd(int no);
+int primeno(int no);
+int symbol(int no);
